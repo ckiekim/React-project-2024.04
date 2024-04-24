d5118017ce7fa50b22cc1d6258bc1f456cffcee1
Cheolkie Kim <ckiekim@naver.com>
2024-04-24 11:21:47 +0900

    Day 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Outlet } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Header from './components/common/Header';
-import Aside from './components/common/Aside';
+import './global.css';
 
-const queryClient = new QueryClient();
+import { useScrollToTop } from './hooks/use-scroll-to-top';
 
-function App() {
+import Router from './routes/sections';
+import ThemeProvider from './theme';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  useScrollToTop();
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Header />
-      <Aside />
-      <Outlet />
-    </QueryClientProvider>
+    <ThemeProvider>
+      <Router />
+    </ThemeProvider>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import App from './App';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
-const router = createBrowserRouter([
-  {
-    path: '/', element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-    ]
-  }
-]);
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <RouterProvider router={router} />
+  <HelmetProvider>
+    <BrowserRouter>
+      <Suspense>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  </HelmetProvider>
 );
