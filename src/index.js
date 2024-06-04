debc5042a30b48a06135a08c597e7be536e88e35
Cheolkie Kim <ckiekim@naver.com>
2024-06-04 09:33:59 +0900

    배포

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <HelmetProvider>
-    <BrowserRouter>
+    <BrowserRouter basename='ck-react-world'>
       <Suspense>
         <App />
       </Suspense>
