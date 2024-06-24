59f83ebc1ad4d07d55d0b541f0f102cb9d15b287
Cheolkie Kim <ckiekim@naver.com>
2024-06-24 21:18:34 +0900

    Day 39

--- src/sections/products/product-cart-widget.jsx
+++ src/sections/products/product-cart-widget.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { styled } from '@mui/material/styles';
